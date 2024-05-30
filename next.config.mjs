7bc8bb4d6b17fa50d5b951241b939e29cf25e1b2
c0smocat <50526934+C0smoCat@users.noreply.github.com>
2024-05-30 18:07:26 +0400

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "/haka-next/"
+  output: "export",
+  distDir: "docs/",
+  basePath: "/haka-next"
 };
 
 export default nextConfig;
