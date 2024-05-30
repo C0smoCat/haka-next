515eebb2d45a27563c184148ffa09dece4c031e5
c0smocat <50526934+C0smoCat@users.noreply.github.com>
2024-05-30 18:20:19 +0400

    upd

---
+++ docs/_next/static/MeSOBYnGYya96h0qR-OLw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/494-85947b76986bb090.js","static/css/a3a621036536dae8.css","static/chunks/pages/index-691a3804263a07b2.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
